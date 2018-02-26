f94d6b7289215bfe7e9457f130474d7046b1fcf5
Ellie <elliewem@gmail.com>
2018-02-26 16:29:59 +0000

    Refactor AccountHistoryLog

--- src/AccountHistoryLog.js
+++ src/AccountHistoryLog.js
@@ -6,11 +6,17 @@
   }
 
   AccountHistoryLog.prototype.storeDebit = function (amount, balance, date = new Date()) {
-    this.log.push({date: date, credit: amount, debit: "-", balance: balance})
+    this.log.push({date: date,
+                   credit: amount,
+                   debit: "-",
+                   balance: balance})
   };
 
   AccountHistoryLog.prototype.storeCredit = function (amount, balance, date = new Date()) {
-    this.log.push({date: date, credit: "-", debit: amount, balance: balance})
+    this.log.push({date: date,
+                   credit: "-",
+                   debit: amount,
+                   balance: balance})
   };
 
   exports.AccountHistoryLog = AccountHistoryLog
